273fe39563d7d87084e2c17edfe70cd0f7e3f05c
Kruvxyz <golaiet@gmail.com>
2023-10-15 10:53:29 +0300

    adding unit tests for shared resources

---
+++ tests/unit/__init__.py
@@ -0,0 +1,5 @@
+import unittest
+from tests.unit.shared.testShared import TestSahred
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/unit/shared/testShared.py
@@ -0,0 +1,38 @@
+import unittest
+import time
+from shared_resources.shared import Shared
+
+
+class TestSahred(unittest.TestCase):
+    def test_question(self):
+        question = "test"
+        shared = Shared()
+        id = shared.add_question(question)
+        verify_id, verify_question = shared.queue[0]
+        self.assertEqual(question, verify_question)
+        self.assertEqual(id, verify_id)
+
+        verify_id, verify_question = shared.get_question()
+        self.assertEqual(question, verify_question)
+        self.assertEqual(id, verify_id)
+
+        obj = shared.get_question()
+        self.assertEqual(None, obj)
+
+    def test_response(self):
+        shared = Shared()
+        id = shared.get_id(1)
+        self.assertEqual(None, shared.responses[id])
+        self.assertEqual({"status": "wait"}, shared.get_response(id))
+        time.sleep(3)
+        self.assertEqual({"status": "Unknown"}, shared.get_response(id))
+
+        expected_response = "test"
+        shared.update_response((id, expected_response), 1)
+        self.assertEqual(
+            {"status": "ok", "answer": expected_response}, shared.get_response(id))
+        self.assertEqual({"status": "Unknown"}, shared.get_response(id))
+
+
+if __name__ == '__main__':
+    unittest.main()
